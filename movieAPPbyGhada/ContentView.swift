897690ae57ee54a870166367dff1b8924a25911f
ghadasaw21 <academyghadaphone@icloud.com>
2025-01-30 14:16:30 +0300

    Initial Commit

---
+++ movieAPPbyGhada/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movieAPPbyGhada
+//
+//  Created by Ghada al ajmi on 30/07/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movieAPPbyGhada/movieAPPbyGhadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieAPPbyGhadaApp.swift
+//  movieAPPbyGhada
+//
+//  Created by Ghada al ajmi on 30/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct movieAPPbyGhadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
